9cb7f9057c42f070badfad6d303f845921aba04a
DavidRealpe <48836443+DavidRealpe@users.noreply.github.com>
2019-04-02 16:06:32 -0500

    Aun sin terminar

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,6 +1,24 @@
 package ui;
 
+import model.Greetings;
+
  public class Main{
+
+ 	public static void main(String[] args){
+ 		//greetings();	
+ 	}
+
+
+ 	public static void greetings(){
+ 		Greetings g = new Greetings(args, "Realpe");
+
+ 		String hi = g.sayHi();
+ 		String gm = g.sayGoodMorning();
+
+ 		System.out.println(hi);
+ 		System.out.println(gm);
+	}
+
 
  	
 
