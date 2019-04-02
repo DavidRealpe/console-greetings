b9406a9dafe8faba7f1b215e1c14cf7a1d2e535b
DavidRealpe <48836443+DavidRealpe@users.noreply.github.com>
2019-04-02 14:41:23 -0500

    Added first version greetings class

---
+++ src/model/Greetings.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Greetings{
+
+	private String name;
+	private String lastName;
+
+	public Greetings(String name, String lastName){
+		this.name = name;
+		this.lastName = lastName;
+	}
+
+	public String sayHi(){
+		return "Hello "+name;
+	}
+
+	public String sayGoodMorning(){
+		return "Good Morning "+name+" "+lastName;
+	}
+
+	public String sayGoodBye(){
+		return "Good Bye"+lastName;
+	}
+
+}
